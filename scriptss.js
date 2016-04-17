667af9e9c69296fb94a73d5bd8eecb06d9957372
Piyush Agrawal <poush12@gmail.com>
2016-04-18 00:11:25 +0530

    Packagist Api fix

--- scriptss.js
+++ scriptss.js
@@ -80,7 +80,7 @@ $(document).ready(function(){
         var q = $(this).val();
         if(q.length <= 2)
             return 0
-        $.get('http://internal-api.laragist.org/v1/?q='+q).done(function(data){
+        $.get('https://packagist.org/search.json?q='+q).done(function(data){
             $('.error').addClass('hidden')
             $('#brand-title').html(" Search Results...")
             $('.no-content').hide()
@@ -238,6 +238,23 @@ function alignBottom(){
 function prepareSnippets(data){
     var string = '<div class="col-md-4 col-sm-6 blog-masonry-item Tech" ><div class="item-inner quote-post"><div class="post-title"><span class="hidden repo">{{repo}}</span><h1 style="margin-bottom: 20px;">{{name}}</h1> <h4 style="margin-bottom: 20px;">{{meta}}<p><br></p></h4><div class="post-meta"> <span class="sub alt-font">Downloads: {{total}}</span></div><a href="#" class="link-text" data-toggle="modal" data-target="#submitModal" >Submit</a></div></div></div>'
     var final = ""
+    $.each(data.results,function(index,value){
+        string = string.replace('{{name}}',value.name);
+        string = string.replace('{{meta}}',value.description);
+        string = string.replace('{{total}}',value.downloads);
+        string = string.replace('{{repo}}',value.name);
+        final += string;
+        string = '<div class="col-md-4 col-sm-6 blog-masonry-item Tech" ><div class="item-inner quote-post"><div class="post-title"><span class="hidden repo">{{repo}}</span><h1 style="margin-bottom: 20px;">{{name}}</h1> <h4 style="margin-bottom: 20px;">{{meta}}<p><br></p></h4><div class="post-meta"> <span class="sub alt-font">Downloads: {{total}}</span></div><a href="#" class="link-text" data-toggle="modal" data-target="#submitModal" >Submit</a></div></div></div>'
+
+    })
+
+    return final;
+}
+
+
+function prepareSnippetsIndex(data){
+    var string = '<div class="col-md-4 col-sm-6 blog-masonry-item Tech" ><div class="item-inner quote-post"><div class="post-title"><span class="hidden repo">{{repo}}</span><h1 style="margin-bottom: 20px;">{{name}}</h1> <h4 style="margin-bottom: 20px;">{{meta}}<p><br></p></h4><div class="post-meta"> <span class="sub alt-font">Downloads: {{total}}</span></div><a href="#" class="link-text" data-toggle="modal" data-target="#submitModal" >Submit</a></div></div></div>'
+    var final = ""
     $.each(data.data,function(index,value){
         string = string.replace('{{name}}',value.name);
         string = string.replace('{{meta}}',value.description);
@@ -251,8 +268,9 @@ function prepareSnippets(data){
     return final;
 }
 
+
 function init(){
     $.get('http://internal-api.laragist.org/v1/').done(function (data) {
-        $('.blog-masonry-container').html(prepareSnippets(data))
+        $('.blog-masonry-container').html(prepareSnippetsIndex(data))
     })
 }
