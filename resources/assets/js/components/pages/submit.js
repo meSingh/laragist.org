ccc1e76b0e64a40296e8b25d9cb1d3e4f10eb71d
Piyush Agrawal <poush12@gmail.com>
2016-04-24 19:43:20 +0530

    Submit Api added

--- resources/assets/js/components/pages/submit.js
+++ resources/assets/js/components/pages/submit.js
@@ -6,20 +6,38 @@ module.exports = {
 
     data: function(){
     	return {
-    		gists: [],
-    		q: ''
+    		gists       : [],
+    		q           : '',
+            notFound    : false,
+            selectedGist: {
+
+                name        : '',
+                first_name  : '',
+                email       : '',
+                category_id : 0    
+            }
     	};
     },
 
     methods: {
     	search: function(){
+        this.notFound = false;
+
     		if(this.q.length < 3)
     			return;
     		// GET request
-      		this.$http({url: 'http://internal-api.laragist.org/v1/?q='+this.q, method: 'GET'}).then(function (response) {
+        
+          
+      		this.$http({url: 'https://packagist.org/search.json?q='+this.q, method: 'GET'}).then(function (response) {
 
-      			if(response.data.meta.pagination.total >0)
-      				this.gists = response.data.data;
+      			if(response.data.total >0)
+      				this.gists = response.data.results;
+            else
+              {
+                this.notFound = true;
+                this.gists = [];
+              }
+
 
     			console.log(response)
 
@@ -27,7 +45,28 @@ module.exports = {
           		// error callback
       		}).bind(this);
 
-    	}
+    	},
+
+        selectGist: function(gist){
+            this.selectedGist.name = gist.name
+
+        },
+
+        submitPackage: function(){
+            client({path: '/submit' ,entity:this.selectedGist}).then(
+                function(response){
+                
+                // success
+                
+                },
+                function(response){
+                    console.log(response)
+                // error
+
+                }
+            )
+        }
+
     }
 
 }
