ba1832df00c9d556d3979f90fe6ddcf2bd8bbf90
Piyush Agrawal <poush12@gmail.com>
2016-05-06 23:09:05 +0530

    Button on support

--- resources/assets/js/components/pages/support.js
+++ resources/assets/js/components/pages/support.js
@@ -11,11 +11,8 @@ module.exports = {
 	methods: {
 		sendEmail : function(){
 
-			var resource = this.$resource('https://us13.api.mailchimp.com/3.0/{/path}');
-			Vue.http.headers.common['Authorization'] = 'Basic YXBpa2V5OjY1NGMzOTBiNmVhYWNhZjA4MzY4YWYwYmMxZDRhMTRjLXVzMTM=';
-			resource.save({path: "lists/be0bdaaf87/members"}, {item:this.subscriber})
-			.then(function(response){
-
+			client({path: '/subscribers', entity: this.subscriber}).then(function(response){
+                
 			})
 		}
 	}
